14078c9aeffe2d052ca6a4a8d3c1e24bda967467
g-nurk <gatlin.nurk@gmail.com>
2024-10-30 14:04:41 +0200

    All assignments added for Sprint 1

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,39 @@
-/**
- * This is an example file and approach for POM in Cypress
- */
 import IssueModal from "../../pages/IssueModal";
+const issueTitle = "This is an issue of type: Task.";
 
-describe('Issue delete', () => {
+describe("Issue delete", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        cy.contains(issueTitle).click();
+      });
   });
 
-  //issue title, that we are testing with, saved into variable
-  const issueTitle = 'This is an issue of type: Task.';
+  /*I added in the IssueModal.js:
+    validateNumberOfIssuesInBacklog(expectedIssuesAfterDeletingOne) {
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.get('[data-testid="list-issue"]').should(
+        "have.length",
+        expectedIssuesAfterDeletingOne
+      );
+    }); */
 
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
+  it("Should delete issue successfully", () => {
+    const expectedIssuesAfterDeletingOne = 3;
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+    IssueModal.validateNumberOfIssuesInBacklog(expectedIssuesAfterDeletingOne);
   });
 
-  it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+  it("Should cancel deletion process successfully", () => {
+    const expectedIssuesAfterCancelling = 4;
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
+    IssueModal.validateNumberOfIssuesInBacklog(expectedIssuesAfterCancelling);
   });
 });
